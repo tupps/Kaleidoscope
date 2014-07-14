021f2268ff259033cc0c2e5e0a0d2aedfa50643c
Tupps <tupps@tupps.com>
2014-07-14 11:08:34 +1000

    Got basic hexagon showing

--- Kaleidoscope/tppViewController.m
+++ Kaleidoscope/tppViewController.m
@@ -13,8 +13,8 @@
     CALayer *imageContainer;
     CALayer *imageMask;
 
-
-    CALayer *debugLastLayer;
+    CAReplicatorLayer *hexReplication1;
+    CAReplicatorLayer *hexReplication2;
 }
 
 
@@ -23,7 +23,7 @@
 
 @implementation tppViewController
 
-static const CGFloat triangleWidth = 25.0;
+static const CGFloat triangleWidth = 50.0;
 static const CGFloat triangleHeight = triangleWidth * 0.866025;
 
 static const CGFloat numberOfHexagonsAcross = 5.0;
@@ -36,10 +36,6 @@ static const CGFloat numberOfHexagonsDown = 7.0;
     imageContainer.contents = (id)[UIImage imageNamed:@"album_cover.jpg"].CGImage;
     imageContainer.bounds = CGRectMake(0.0, 0.0, 350.0, 350.0); //Cheated, copied from image properties
 
-    imageMask = [CALayer layer];
-    imageMask.bounds = CGRectMake(0.0, 0.0, triangleWidth, triangleHeight);
-    [imageMask addSublayer:imageContainer];
-
     CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
     UIBezierPath *imageMarkBezierPath = [UIBezierPath bezierPath];
     [imageMarkBezierPath moveToPoint:CGPointMake(0.0, triangleHeight)]; //Bottom Left
@@ -50,22 +46,52 @@ static const CGFloat numberOfHexagonsDown = 7.0;
     [maskLayer setPath:imageMarkBezierPath.CGPath];
     [maskLayer setFillColor:[[UIColor whiteColor] CGColor]];
 
+
+    imageMask = [CALayer layer];
+    imageMask.bounds = CGRectMake(0.0, 0.0, triangleWidth, triangleHeight);
+    [imageMask addSublayer:imageContainer];
     imageMask.mask = maskLayer;
 
-    [self.view.layer addSublayer:imageMask];
+    hexReplication1 = [CAReplicatorLayer layer];
+    hexReplication1.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0);
+    hexReplication1.instanceCount = 2;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DRotate(transform, M_PI, 0.0, 1.0, 0.0);
+    transform = CATransform3DRotate(transform, 60 * M_PI / 180, 0.0, 0.0, 1.0);
+    hexReplication1.instanceTransform = transform;
+    [hexReplication1 addSublayer:imageMask];
+    imageMask.position = CGPointMake(triangleWidth, triangleHeight * 1.5);
+
+    hexReplication2 = [CAReplicatorLayer layer];
+    hexReplication2.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0);
+    hexReplication2.instanceCount = 3;
+    transform = CATransform3DIdentity;
+    transform = CATransform3DRotate(transform, 120 * M_PI / 180, 0.0, 0.0, 1.0);
+    hexReplication2.instanceTransform = transform;
+    [hexReplication2 addSublayer:hexReplication1];
+    hexReplication1.position = CGPointMake(triangleWidth, triangleHeight);
+
+    hexReplication2.backgroundColor = [UIColor greenColor].CGColor;
+    [self.view.layer addSublayer:hexReplication2];
 }
 
 - (void) viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
 
-    CABasicAnimation *basicAnimation = [CABasicAnimation animation];
-    basicAnimation
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
+    animation.duration = 10;
+    animation.additive = YES;
+    animation.removedOnCompletion = NO;
+    animation.fillMode = kCAFillModeForwards;
+    animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(0.0, 160.0)];
+    animation.toValue = [NSValue valueWithCGPoint:CGPointMake(160.0, 0.0)];
+    [imageContainer addAnimation:animation forKey:@"basicMovement"];
 }
 
 - (void) viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
 
-    imageMask.position = CGPointMake(100.0, 100.0);
+    hexReplication2.position = CGPointMake(100.0, 100.0);
 }
 
 
