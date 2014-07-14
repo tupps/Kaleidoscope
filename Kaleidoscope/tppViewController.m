66fe0733f51ace66ed5d0eb53118c99aabea594b
Tupps <tupps@tupps.com>
2014-07-14 10:50:07 +1000

    Triangle Displayed

--- Kaleidoscope/tppViewController.m
+++ Kaleidoscope/tppViewController.m
@@ -7,23 +7,66 @@
 //
 
 #import "tppViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
-@interface tppViewController ()
+@interface tppViewController () {
+    CALayer *imageContainer;
+    CALayer *imageMask;
+
+
+    CALayer *debugLastLayer;
+}
+
+
 
 @end
 
 @implementation tppViewController
 
-- (void)viewDidLoad
-{
+static const CGFloat triangleWidth = 25.0;
+static const CGFloat triangleHeight = triangleWidth * 0.866025;
+
+static const CGFloat numberOfHexagonsAcross = 5.0;
+static const CGFloat numberOfHexagonsDown = 7.0;
+
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    imageContainer = [CALayer layer];
+    imageContainer.contents = (id)[UIImage imageNamed:@"album_cover.jpg"].CGImage;
+    imageContainer.bounds = CGRectMake(0.0, 0.0, 350.0, 350.0); //Cheated, copied from image properties
+
+    imageMask = [CALayer layer];
+    imageMask.bounds = CGRectMake(0.0, 0.0, triangleWidth, triangleHeight);
+    [imageMask addSublayer:imageContainer];
+
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    UIBezierPath *imageMarkBezierPath = [UIBezierPath bezierPath];
+    [imageMarkBezierPath moveToPoint:CGPointMake(0.0, triangleHeight)]; //Bottom Left
+    [imageMarkBezierPath addLineToPoint:CGPointMake(triangleWidth / 2.0, 0.0)]; //Top Center
+    [imageMarkBezierPath addLineToPoint:CGPointMake(triangleWidth, triangleHeight)]; //Bottom Right
+    [imageMarkBezierPath closePath];
+
+    [maskLayer setPath:imageMarkBezierPath.CGPath];
+    [maskLayer setFillColor:[[UIColor whiteColor] CGColor]];
+
+    imageMask.mask = maskLayer;
+
+    [self.view.layer addSublayer:imageMask];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+
+    CABasicAnimation *basicAnimation = [CABasicAnimation animation];
+    basicAnimation
 }
+
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+
+    imageMask.position = CGPointMake(100.0, 100.0);
+}
+
 
 @end
