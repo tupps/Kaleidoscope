f12db1f6234d15d713bac084d7b8fb30235e36c7
Tupps <tupps@tupps.com>
2014-07-14 11:41:36 +1000

    Updated with rotation as well as movement animations

--- Kaleidoscope/tppViewController.m
+++ Kaleidoscope/tppViewController.m
@@ -15,9 +15,11 @@
 
     CAReplicatorLayer *hexReplication1;
     CAReplicatorLayer *hexReplication2;
+
+    CAReplicatorLayer *hexDownReplicator;
+    CAReplicatorLayer *hexRightReplicator1;
+    CAReplicatorLayer *hexRightReplicator2;
 }
-
-
 
 @end
 
@@ -32,47 +34,15 @@ static const CGFloat numberOfHexagonsDown = 7.0;
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    imageContainer = [CALayer layer];
-    imageContainer.contents = (id)[UIImage imageNamed:@"album_cover.jpg"].CGImage;
-    imageContainer.bounds = CGRectMake(0.0, 0.0, 350.0, 350.0); //Cheated, copied from image properties
+    [self createImage];
+    [self createTriangleImageMask];
+    [self createOneThirdOfHexagon];
+    [self completeHexagon];
+    [self replicateHexagonsDown];
+    [self replicateDownHexagonsRight];
+    [self replicateRightHexagonsRight];
 
-    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
-    UIBezierPath *imageMarkBezierPath = [UIBezierPath bezierPath];
-    [imageMarkBezierPath moveToPoint:CGPointMake(0.0, triangleHeight)]; //Bottom Left
-    [imageMarkBezierPath addLineToPoint:CGPointMake(triangleWidth / 2.0, 0.0)]; //Top Center
-    [imageMarkBezierPath addLineToPoint:CGPointMake(triangleWidth, triangleHeight)]; //Bottom Right
-    [imageMarkBezierPath closePath];
-
-    [maskLayer setPath:imageMarkBezierPath.CGPath];
-    [maskLayer setFillColor:[[UIColor whiteColor] CGColor]];
-
-
-    imageMask = [CALayer layer];
-    imageMask.bounds = CGRectMake(0.0, 0.0, triangleWidth, triangleHeight);
-    [imageMask addSublayer:imageContainer];
-    imageMask.mask = maskLayer;
-
-    hexReplication1 = [CAReplicatorLayer layer];
-    hexReplication1.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0);
-    hexReplication1.instanceCount = 2;
-    CATransform3D transform = CATransform3DIdentity;
-    transform = CATransform3DRotate(transform, M_PI, 0.0, 1.0, 0.0);
-    transform = CATransform3DRotate(transform, 60 * M_PI / 180, 0.0, 0.0, 1.0);
-    hexReplication1.instanceTransform = transform;
-    [hexReplication1 addSublayer:imageMask];
-    imageMask.position = CGPointMake(triangleWidth, triangleHeight * 1.5);
-
-    hexReplication2 = [CAReplicatorLayer layer];
-    hexReplication2.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0);
-    hexReplication2.instanceCount = 3;
-    transform = CATransform3DIdentity;
-    transform = CATransform3DRotate(transform, 120 * M_PI / 180, 0.0, 0.0, 1.0);
-    hexReplication2.instanceTransform = transform;
-    [hexReplication2 addSublayer:hexReplication1];
-    hexReplication1.position = CGPointMake(triangleWidth, triangleHeight);
-
-    hexReplication2.backgroundColor = [UIColor greenColor].CGColor;
-    [self.view.layer addSublayer:hexReplication2];
+    [self.view.layer addSublayer:hexRightReplicator2];
 }
 
 - (void) viewDidAppear:(BOOL)animated {
@@ -86,12 +56,104 @@ static const CGFloat numberOfHexagonsDown = 7.0;
     animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(0.0, 160.0)];
     animation.toValue = [NSValue valueWithCGPoint:CGPointMake(160.0, 0.0)];
     [imageContainer addAnimation:animation forKey:@"basicMovement"];
+
+    CABasicAnimation *animation2 = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    animation2.duration = 10;
+    animation2.additive = YES;
+    animation2.removedOnCompletion = NO;
+    animation2.fillMode = kCAFillModeForwards;
+    animation2.fromValue = [NSNumber numberWithDouble:0.0];
+    animation2.toValue = [NSNumber numberWithDouble:M_PI];
+    [imageContainer addAnimation:animation2 forKey:@"basicMovement"];
 }
 
 - (void) viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
+    hexRightReplicator2.frame = self.view.bounds;
+}
 
-    hexReplication2.position = CGPointMake(100.0, 100.0);
+#pragma mark - Image Setup
+
+- (void)createImage {
+    imageContainer = [CALayer layer];
+    imageContainer.contents = (id)[UIImage imageNamed:@"album_cover.jpg"].CGImage;
+    imageContainer.bounds = CGRectMake(0.0, 0.0, 800.0, 800.0);  //Cheated, copied from image properties
+}
+
+- (void)createTriangleImageMask {
+    CAShapeLayer *maskLayer = [[CAShapeLayer alloc] init];
+    UIBezierPath *imageMaskBezierPath = [UIBezierPath bezierPath];
+    [imageMaskBezierPath moveToPoint:CGPointMake(0.0, triangleHeight)]; //Bottom Left
+    [imageMaskBezierPath addLineToPoint:CGPointMake(triangleWidth / 2.0, 0.0)]; //Top Center
+    [imageMaskBezierPath addLineToPoint:CGPointMake(triangleWidth, triangleHeight)]; //Bottom Right
+    [imageMaskBezierPath closePath];
+
+    [maskLayer setPath:imageMaskBezierPath.CGPath];
+    [maskLayer setFillColor:[[UIColor whiteColor] CGColor]];
+
+
+    imageMask = [CALayer layer];
+    imageMask.bounds = CGRectMake(0.0, 0.0, triangleWidth, triangleHeight);
+    [imageMask addSublayer:imageContainer];
+    imageMask.mask = maskLayer;
+}
+
+- (void)createOneThirdOfHexagon {
+    hexReplication1 = [CAReplicatorLayer layer];
+    hexReplication1.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0);
+    hexReplication1.instanceCount = 2;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DRotate(transform, M_PI, 0.0, 1.0, 0.0);
+    transform = CATransform3DRotate(transform, 60 * M_PI / 180, 0.0, 0.0, 1.0);
+    hexReplication1.instanceTransform = transform;
+    [hexReplication1 addSublayer:imageMask];
+    imageMask.position = CGPointMake(triangleWidth, triangleHeight * 1.5);
+}
+
+- (void)completeHexagon {
+    hexReplication2 = [CAReplicatorLayer layer];
+    hexReplication2.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0);
+    hexReplication2.instanceCount = 3;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DRotate(transform, 120 * M_PI / 180, 0.0, 0.0, 1.0);
+    hexReplication2.instanceTransform = transform;
+    [hexReplication2 addSublayer:hexReplication1];
+    hexReplication1.position = CGPointMake(triangleWidth, triangleHeight);
+}
+
+- (void)replicateHexagonsDown {
+    hexDownReplicator = [CAReplicatorLayer layer];
+    hexDownReplicator.bounds = CGRectMake(0.0, 0.0, triangleWidth * 2.0, triangleHeight * 2.0 * numberOfHexagonsDown);
+    hexDownReplicator.instanceCount = numberOfHexagonsDown;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DTranslate(transform, 0.0, triangleHeight * 2.0, 0.0);
+    hexDownReplicator.instanceTransform = transform;
+    [hexDownReplicator addSublayer:hexReplication2];
+    hexDownReplicator.position = CGPointMake(triangleWidth, triangleHeight);
+}
+
+- (void) replicateDownHexagonsRight {
+    hexRightReplicator1 = [CAReplicatorLayer layer];
+    NSInteger numberOfRows = ceil(numberOfHexagonsAcross / 2.0);
+    hexRightReplicator1.bounds = CGRectMake(0.0, 0.0, triangleWidth * 3.0 * numberOfRows, triangleHeight * 2.0 * numberOfHexagonsDown);
+    hexRightReplicator1.instanceCount = numberOfRows;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DTranslate(transform, triangleWidth * 3.0, 0.0, 0.0);
+    hexRightReplicator1.instanceTransform = transform;
+    [hexRightReplicator1 addSublayer:hexDownReplicator];
+    hexDownReplicator.position = CGPointMake(triangleWidth, triangleHeight * 2.0 * numberOfHexagonsDown / 2.0);
+}
+
+- (void) replicateRightHexagonsRight {
+    hexRightReplicator2 = [CAReplicatorLayer layer];
+    NSInteger numberOfRows = floor(numberOfHexagonsAcross / 2.0);
+    hexRightReplicator2.bounds = CGRectMake(0.0, 0.0, triangleWidth * 3.0 * numberOfRows, triangleHeight * 2.0 * numberOfHexagonsDown);
+    hexRightReplicator2.instanceCount = numberOfRows;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DTranslate(transform, triangleWidth * 1.5, triangleHeight, 0.0);
+    hexRightReplicator2.instanceTransform = transform;
+    [hexRightReplicator2 addSublayer:hexRightReplicator1];
+    hexRightReplicator1.position = CGPointMake(triangleWidth * 3.0 * numberOfRows / 2.0, triangleHeight * 2.0 * numberOfHexagonsDown / 2.0);
 }
 
 
